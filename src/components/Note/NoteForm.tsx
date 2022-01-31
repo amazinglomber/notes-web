bf1ee5e984b03b42b9690aa189cdbf64726b5893
lomber <tomek.niezurawski@gmail.com>
2022-01-31 07:03:22 +0100

    Added data validation

--- src/components/Note/NoteForm.tsx
+++ src/components/Note/NoteForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import NoteTitleField from './NoteTitleField';
 import NoteBodyField from './NoteBodyField';
+import { FormControl, FormHelperText } from '@mui/material';
 
 export interface NoteDetailsProps {
   note?: INote;
@@ -16,39 +17,80 @@ const NoteForm: React.FC<NoteDetailsProps> = ({ note, onChange }) => {
   const [title, setTitle] = useState(note?.title ?? '');
   const [body, setBody] = useState(note?.body ?? '');
 
+  const [titleError, setTitleError] = useState(false);
+  const [bodyError, setBodyError] = useState(false);
+
   const handleChange = (field: FieldType) => (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     const value = event.target.value;
 
+    let error = false;
+
     switch (field) {
       case 'title':
-        setTitle(value);
+        if (value.length <= 200) {
+          setTitle(value);
+        } else {
+          setTitleError(true);
+          error = true;
+        }
+
         break;
       case 'body':
-        setBody(value);
+        if (body.length <= 2000) {
+          setBody(value);
+        } else {
+          setBodyError(true);
+          error = true;
+        }
+
+        break;
     }
 
-    const createNote: ICreateNote = {
-      title,
-      body,
-    };
-    createNote[field] = value;
-    onChange(createNote);
+    if (!error) {
+      const createNote: ICreateNote = {
+        title,
+        body,
+      };
+      createNote[field] = value.substring(0, field === 'title' ? 200 : 2000);
+      onChange(createNote);
+    }
   }
 
   return (
     <div>
-      <NoteTitleField
-        placeholder={t('form.note.placeholder.title')}
-        value={title}
-        onChange={handleChange('title')}
-      />
+      <FormControl
+        variant="standard"
+        fullWidth
+        error={titleError}
+      >
+        <NoteTitleField
+          placeholder={t('form.note.placeholder.title')}
+          value={title}
+          onChange={handleChange('title')}
+        />
+        <FormHelperText>
+          {titleError && t('form.note.validationError.title')}
+        </FormHelperText>
+      </FormControl>
+
       <br />
-      <NoteBodyField
-        multiline
-        placeholder={t('form.note.placeholder.body')}
-        value={body}
-        onChange={handleChange('body')}
-      />
+
+      <FormControl
+        variant="standard"
+        fullWidth
+        error={bodyError}
+      >
+        <NoteBodyField
+          multiline
+          fullWidth
+          placeholder={t('form.note.placeholder.body')}
+          value={body}
+          onChange={handleChange('body')}
+        />
+        <FormHelperText>
+          {bodyError && t('form.note.validationError.body')}
+        </FormHelperText>
+      </FormControl>
     </div>
   );
 };
--- src/components/Note/NoteTitleField.tsx
+++ src/components/Note/NoteTitleField.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { InputBase, InputBaseProps, styled } from '@mui/material';
+import { InputBase, InputBaseProps, styled, useTheme } from '@mui/material';
 
-const NoteTitleField = styled(InputBase)(({ theme }) => ({
-  "& .MuiInputBase-input": {
-    ...theme.typography.h5
-  }
-}))
+const NoteTitleField: React.FC<InputBaseProps> = ({ fullWidth, multiline, onChange, ...other }) => {
+  const theme = useTheme();
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    // Prevent from inserting newline characters.
+    event.target.value = event.target.value.replace(/[\r\n\v]+/g, "");
+
+    if (onChange) {
+      onChange(event);
+    }
+  };
+
+  return (
+    <InputBase
+      sx={{
+        ...theme.typography.h5
+      }}
+      fullWidth={fullWidth ?? true}
+      multiline={multiline ?? true}
+      onChange={handleChange}
+      {...other}
+    />
+  );
+};
 
 export default NoteTitleField;
